04d22b3e7d6b7b6c7d5d99bb555575a07a1c638b
Nurullah <beepbeepwhoisthejeep@gmail.com>
2019-11-13 21:46:13 +0300

    commit

--- sailcovar3.jl
+++ sailcovar3.jl
@@ -8,20 +8,25 @@ m = Model(with_optimizer(Clp.Optimizer))
 
 @variable(m, 0 <= x[1:5] <= 40)       # boats produced with regular labor
 @variable(m, y[1:5] >= 0)             # boats produced with overtime labor
-@variable(m, h[1:6] >= 0)             # boats held in inventory
+@variable(m, hp[1:5] >= 0)             # boats held in inventory
+@variable(m, hn[1:5] >= 0)
 @variable(m, p[1:4] >= 0)
 @variable(m, n[1:4] >= 0)
+@variable(m, hcombine[1:5])
 @variable(m, combine[1:4])
-@constraint(m, h[1] == 10)
-@constraint(m, h[5] >= 10)
+@constraint(m, hp[5] >= 10)
+@constraint(m, hn[5] <= 0)
 
 @constraint(m, x[1]+y[1]==50)
+@constraint(m, hp[1]-hn[1]==10)
+
 @constraint(m,flow3[i in 1:4],x[i+1]+y[i+1]-(x[i]+y[i])==combine[i])
 @constraint(m, flow2[i in 1:4], p[i]-n[i]==combine[i])
+@constraint(m, flow4[i in 1:5], hp[i]-hn[i]==hcombine[i])
 
-@constraint(m, flow[i in 1:4], h[i]+x[i+1]+y[i+1]==d[i+1]+h[i+1])     # conservation of boats x[i+1]+y[i+1]-x[i]+y[i]==p[i+1]-n[i+1]
+@constraint(m, flow[i in 1:4], hcombine[i]+x[i+1]+y[i+1]==d[i+1]+hcombine[i+1])     # conservation of boats x[i+1]+y[i+1]-x[i]+y[i]==p[i+1]-n[i+1]
 
-@objective(m, Min, 400*sum(x) + 450*sum(y) + 20*sum(h) + 400*sum(p)+500*sum(n))         # minimize costs
+@objective(m, Min, 400*sum(x) + 450*sum(y) + 20*sum(hp) + 400*sum(p)+500*sum(n) + 100*sum(hn))         # minimize costs
 
 optimize!(m)
 
@@ -38,6 +43,8 @@ end
 
 println("Build ", Array{Int}(value.(x')), " using regular labor")
 println("Build ", Array{Int}(value.(y')), " using overtime labor")
-println("Inventory: ", Array{Int}(value.(h')))
+println("Inventory: ", Array{Int}(value.(hcombine')))
+println("h+ ",Array{Int}(value.(hp')))
+println("h- ",Array{Int}(value.(hn')))
 println("c+ ",Array{Int}(value.(p')))
 println("c- ",Array{Int}(value.(n')))
