8eedfcd333672831af7c0fe66cbb9d29d1fc6a71
Nurullah <beepbeepwhoisthejeep@gmail.com>
2019-11-13 23:56:48 +0300

    commit

--- sailcovar1.jl
+++ sailcovar1.jl
@@ -9,8 +9,8 @@ m = Model(with_optimizer(Clp.Optimizer))
 @variable(m, 0 <= x[1:5] <= 40)       # boats produced with regular labor
 @variable(m, y[1:5] >= 0)             # boats produced with overtime labor
 @variable(m, h[1:6] >= 0)             # boats held in inventory
-@constraint(m, h[2] == 15)            #initially inv Q2 has 15 boots
-@constraint(m, h[6] >= 10)            #min problem thus can use '>=' instead '=='
+@constraint(m, h[2] == 15)            # initially inventory Q2 has 10+40-35 = 15 boots
+@constraint(m, h[6] >= 10)            # min problem thus can use '>=' instead '=='
 @constraint(m, flow[i in 2:5], h[i]+x[i]+y[i]==d[i]+h[i+1])     # Q2-Q5=>i in 2:5
 @objective(m, Min, 400*sum(x) + 450*sum(y) + 20*sum(h))         # minimize costs
 
